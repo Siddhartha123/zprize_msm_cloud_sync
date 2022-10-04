14fe40fa18d2f741cd08406e4f23c5b506d01c71
Ippili Sidhartha Kumar <iskumar789@gmail.com>
2022-10-04 14:30:55 +0530

    two bucket processes sharing padd unit
    initialized  num_padd_ops and count_B without calculation on HW

--- hls_zprize_msm/directives.tcl
+++ hls_zprize_msm/directives.tcl
@@ -9,6 +9,6 @@ set_directive_pipeline -style frp "msm_arr/msm_arr_dataflow_4"
 set_directive_pipeline -off "bucket_unit/bucket_unit_label0"
 # set_directive_pipeline -off "msm_arr/pre_bucket_unit_label0"
 
-set_directive_pipeline -style frp "msm_arr/msm_arr_dataflow_0"
+set_directive_pipeline -style frp "msm_arr/bucket_process/msm_arr_dataflow_0"
 set_directive_pipeline -style frp "msm_arr/msm_arr_dataflow_2"
-set_directive_pipeline -style frp "msm_arr/msm_arr_dataflow_3"
+set_directive_pipeline -style frp "msm_arr/bucket_process/msm_arr_dataflow_3"
--- hls_zprize_msm/src/msm_dataflow.cpp
+++ hls_zprize_msm/src/msm_dataflow.cpp
@@ -1,8 +1,15 @@
 #include "msm.h"
 
-void bucket_unit(hls::stream<bls12_377_coord_k_t> &input_fifo,
-                 hls::stream<bls12_377_coord_k_t> &padd_output_fifo, N_t count_B[16],
-                 hls::stream<dbl_bls12_377_coord_k_t> &CFIFO, bls12_377_coord_t B_i[16]) {
+void bucket_process(N_t count_B[16], N_t num_padd_ops, hls::stream<bls12_377_coord_k_t> &BFIFO,
+                    hls::stream<bls12_377_coord_k_t> &padd_output_fifo,
+                    bls12_377_coord_t GBUFF_P2D[16], hls::stream<dbl_bls12_377_coord_k_t> &CFIFO) {
+#pragma HLS dataflow
+    bls12_377_coord_k_t data;
+    bool valid_data = false;
+    N_t count_if = 0, count_padd_of = 0;
+
+    ap_uint<4> nibble_K;
+
     // ---- Buckets ----
     bool fill_B[16] = {true, true, true, true, true, true, true, true,
                        true, true, true, true, true, true, true, true};
@@ -28,37 +35,20 @@ void bucket_unit(hls::stream<bls12_377_coord_k_t> &input_fifo,
 #pragma HLS STREAM variable = B[14] depth = 1
 #pragma HLS STREAM variable = B[15] depth = 1
 
-    ap_uint<4> nibble_K;
-    fp_t padd_count[16] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-    fp_t debug_ctr = 0;
     hls::stream<bls12_377_coord_k_t> OVFIFO("Overflow fifo");
-    // Pre-bucket FIFOs
+#pragma HLS STREAM variable = OVFIFO depth = 16
+    // ap_uint<4> nibble_K;
+    // bucket-wise padd count
+    fp_t padd_count[16] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
-    N_t count_if = 0, count_padd_of = 0;
-#pragma HLS STREAM variable = OVFIFO depth = 15
-    N_t num_padd_ops = 0;
-    // ---- Determine #padd ops ----
-    for (int i = 1; i < 16; i++) {
-#pragma HLS pipeline II = 1
-        if (count_B[i] != 0) num_padd_ops += (count_B[i] - 1);
-    }
-
-    for (int i = 0; i < NUM_POINTS; i++) {
-        CFIFO.write((input_fifo.read(), padd_output_fifo.read()));
-    }
-
-#pragma HLS dataflow
-    bls12_377_coord_k_t data;
-    bool valid_data = false;
     while ((count_if < NUM_POINTS) || (count_padd_of < num_padd_ops)) {
-#pragma HLS pipeline II = 1
-        if (count_if < NUM_POINTS && !input_fifo.empty()) {
-            data = input_fifo.read();
+        if (count_if < NUM_POINTS && !BFIFO.empty()) {
+            data = BFIFO.read();
             count_if += 1;
             valid_data = true;
             // take care of single entry buckets i.e. count_B==1
             if (count_B[data(NIBBLE_RANGE)] == 1) {
-                B_i[data(NIBBLE_RANGE)] = data(bls12_377_coord_t::width - 1, 0);
+                GBUFF_P2D[data(NIBBLE_RANGE)] = data(bls12_377_coord_t::width - 1, 0);
                 valid_data = false;
             }
         } else if (count_padd_of < num_padd_ops && !padd_output_fifo.empty()) {
@@ -70,7 +60,7 @@ void bucket_unit(hls::stream<bls12_377_coord_k_t> &input_fifo,
             // if last element, put in B_i and change valid to false
             padd_count[data(NIBBLE_RANGE)] += 1;
             if (padd_count[data(NIBBLE_RANGE)] == count_B[data(NIBBLE_RANGE)] - 1) {
-                B_i[data(NIBBLE_RANGE)] = data(1131 - 1, 0);
+                GBUFF_P2D[data(NIBBLE_RANGE)] = data(1131 - 1, 0);
                 valid_data = false;
             }
         } else
@@ -93,13 +83,9 @@ void bucket_unit(hls::stream<bls12_377_coord_k_t> &input_fifo,
         }
     }
 
-    // todo: use splitting as shown in example of ug1399, but conditional data
-    // duplication
     bls12_377_coord_t result;
-    fp_t p_x, p_y, p_z;
-    u32 b_data;
 
-bucket_unit_label0:
+msm_arr_dataflow_3:
     for (int i = 0; i < num_padd_ops; i++) {
         data = OVFIFO.read();
         nibble_K = data(NIBBLE_RANGE);
@@ -108,13 +94,7 @@ bucket_unit_label0:
     }
 }
 
-void calc_num_padd_ops(N_t count_B[16], N_t num_padd_ops) {
-    num_padd_ops = 0;
-    for (int i = 1; i < 16; i++) {
-#pragma HLS pipeline II = 1
-        if (count_B[i] != 0) num_padd_ops += (count_B[i] - 1);
-    }
-}
+// -------------------------------------------------------------------------
 
 void msm_arr(fp_t P_arr_x[NUM_POINTS], fp_t P_arr_y[NUM_POINTS], fp_t P_arr_z[NUM_POINTS],
              fr_t K_arr[NUM_POINTS], u32 B_i[30]) {
@@ -138,147 +118,98 @@ void msm_arr(fp_t P_arr_x[NUM_POINTS], fp_t P_arr_y[NUM_POINTS], fp_t P_arr_z[NU
      */
 
 #pragma HLS dataflow
-    hls::stream<bls12_377_coord_k_t> BFIFO_1("Bucket fifo for chunk position 1");
-    hls::stream<bls12_377_coord_k_t> padd_output_fifo("PADD output fifo");
-#pragma HLS STREAM variable = padd_output_fifo depth = 15
-#pragma HLS STREAM variable = BFIFO_1 depth = 5
     ap_uint<4> nibble_K;
-    hls::stream<dbl_bls12_377_coord_k_t> CFIFO("central fifo");  // PADD FIFOS
-#pragma HLS STREAM variable = CFIFO depth = 15
-    N_t count_B[16] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    N_t cnt_bucket_chunks[16][16] = {
+        {0, 2, 1, 2, 2, 1, 0, 2, 2, 2, 0, 0, 0, 0, 0, 2},
+        {0, 2, 1, 2, 2, 1, 0, 2, 2, 2, 0, 0, 0, 0, 0, 2},
+        {0, 2, 1, 2, 2, 1, 0, 2, 2, 2, 0, 0, 0, 0, 0, 2},
+        {0, 2, 1, 2, 2, 1, 0, 2, 2, 2, 0, 0, 0, 0, 0, 2},
+        {0, 2, 1, 2, 2, 1, 0, 2, 2, 2, 0, 0, 0, 0, 0, 2},
+        {0, 2, 1, 2, 2, 1, 0, 2, 2, 2, 0, 0, 0, 0, 0, 2},
+        {0, 2, 1, 2, 2, 1, 0, 2, 2, 2, 0, 0, 0, 0, 0, 2},
+        {0, 2, 1, 2, 2, 1, 0, 2, 2, 2, 0, 0, 0, 0, 0, 2},
+        {0, 2, 1, 2, 2, 1, 0, 2, 2, 2, 0, 0, 0, 0, 0, 2},
+        {0, 2, 1, 2, 2, 1, 0, 2, 2, 2, 0, 0, 0, 0, 0, 2},
+        {0, 2, 1, 2, 2, 1, 0, 2, 2, 2, 0, 0, 0, 0, 0, 2},
+        {0, 2, 1, 2, 2, 1, 0, 2, 2, 2, 0, 0, 0, 0, 0, 2},
+        {0, 2, 1, 2, 2, 1, 0, 2, 2, 2, 0, 0, 0, 0, 0, 2},
+        {0, 2, 1, 2, 2, 1, 0, 2, 2, 2, 0, 0, 0, 0, 0, 2},
+        {0, 2, 1, 2, 2, 1, 0, 2, 2, 2, 0, 0, 0, 0, 0, 2},
+        {0, 2, 1, 2, 2, 1, 0, 2, 2, 2, 0, 0, 0, 0, 0, 2},
+    };
+    N_t num_padd_ops[16] = {7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7};
 
-    // ---- Buckets ----
-    bool fill_B[16] = {true, true, true, true, true, true, true, true,
-                       true, true, true, true, true, true, true, true};
-    hls::stream<bls12_377_coord_t> B[16];
-    // TODO: cosider increasing the bucket depths, since fill_B logic is independently handling the
-    // point routing logic OR, use bucket empty condition to route points between bucket and ovfifo.
-    // BUT beware of the deadlock condition if there is a point in ovfifo but no point in the
-    // corresponding bucket.
-#pragma HLS STREAM variable = B[0] depth = 1
-#pragma HLS STREAM variable = B[1] depth = 1
-#pragma HLS STREAM variable = B[2] depth = 1
-#pragma HLS STREAM variable = B[3] depth = 1
-#pragma HLS STREAM variable = B[4] depth = 1
-#pragma HLS STREAM variable = B[5] depth = 1
-#pragma HLS STREAM variable = B[6] depth = 1
-#pragma HLS STREAM variable = B[7] depth = 1
-#pragma HLS STREAM variable = B[8] depth = 1
-#pragma HLS STREAM variable = B[9] depth = 1
-#pragma HLS STREAM variable = B[10] depth = 1
-#pragma HLS STREAM variable = B[11] depth = 1
-#pragma HLS STREAM variable = B[12] depth = 1
-#pragma HLS STREAM variable = B[13] depth = 1
-#pragma HLS STREAM variable = B[14] depth = 1
-#pragma HLS STREAM variable = B[15] depth = 1
+    hls::stream<bls12_377_coord_k_t> BFIFO_0("Bucket fifo for chunk position 1"),
+        BFIFO_1("Bucket fifo for chunk position 2");
+    hls::stream<bls12_377_coord_k_t> padd_output_fifo_0("PADD output fifo chunk 1"),
+        padd_output_fifo_1("PADD output fifo chunk 1");
+#pragma HLS STREAM variable = padd_output_fifo_0 depth = 15
+#pragma HLS STREAM variable = padd_output_fifo_1 depth = 15
 
-    // ap_uint<4> nibble_K;
-    fp_t padd_count[16] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-    fp_t debug_ctr = 0;
-    hls::stream<bls12_377_coord_k_t> OVFIFO("Overflow fifo");
-#pragma HLS STREAM variable = OVFIFO depth = 16
+#pragma HLS STREAM variable = BFIFO_0 depth = 5
+#pragma HLS STREAM variable = BFIFO_1 depth = 5
+    hls::stream<dbl_bls12_377_coord_k_t> CFIFO_0("central fifo for chunk 1"),
+        CFIFO_1("central fifo for chunk 1");  // PADD FIFOS
+#pragma HLS STREAM variable = CFIFO_0 depth = 15
+#pragma HLS STREAM variable = CFIFO_1 depth = 15
 
-    N_t count_if = 0, count_padd_of = 0;
-    N_t num_padd_ops = 0;
-
-    bls12_377_coord_k_t data;
-    bool valid_data = false;
-
-    // bucket_unit(BFIFO_1, padd_output_fifo, count_B, CFIFO, GBUFF_P2D[0]);
-
+    N_t ni = 0;
+// for (N_t ni = 0; ni < 16; ni = ni + 1) {
 msm_arr_dataflow_0:
     for (int i = 0; i < NUM_POINTS; i++) {
-        nibble_K = GBUFF_K[i](((0 + 1) << 2) - 1, 0 << 2);
-        count_B[nibble_K] += 1;
-        // BFIFO_1.write((nibble_K, GBUFF_P[i]));
+        nibble_K = GBUFF_K[i](((ni + 1) << 2) - 1, (ni) << 2);
+        BFIFO_0.write((nibble_K, GBUFF_P[i]));
+        nibble_K = GBUFF_K[i](((ni + 1) << 2) - 1, (ni) << 2);
+        BFIFO_1.write((nibble_K, GBUFF_P[i]));
     }
+    // }
 
-    // ---- Determine #padd ops ----
-    // msm_arr_dataflow_1:
-    //     for (int i = 1; i < 16; i++) {
-    // #pragma HLS pipeline II = 1
-    //         if (count_B[i] != 0) num_padd_ops += (count_B[i] - 1);
-    //     }
+    // for (N_t ni = 0; ni < 16; ni = ni + 1)
+    bucket_process(cnt_bucket_chunks[ni], num_padd_ops[ni], BFIFO_0, padd_output_fifo_0,
+                   GBUFF_P2D[0], CFIFO_0);
+    bucket_process(cnt_bucket_chunks[ni + 1], num_padd_ops[ni + 1], BFIFO_1, padd_output_fifo_1,
+                   GBUFF_P2D[1], CFIFO_1);
 
-    num_padd_ops = 7;
-
-    N_t itr_bfifo = 0;
-msm_arr_dataflow_2:
-    while ((itr_bfifo < NUM_POINTS) || (count_if < NUM_POINTS) || (count_padd_of < num_padd_ops)) {
-#pragma HLS dataflow
-
-        if (itr_bfifo < NUM_POINTS) {
-            nibble_K = GBUFF_K[itr_bfifo](((0 + 1) << 2) - 1, 0 << 2);
-            BFIFO_1.write((nibble_K, GBUFF_P[itr_bfifo++]));
-        }
-
-        if (count_if < NUM_POINTS && !BFIFO_1.empty()) {
-            data = BFIFO_1.read();
-            count_if += 1;
-            valid_data = true;
-            // take care of single entry buckets i.e. count_B==1
-            if (count_B[data(NIBBLE_RANGE)] == 1) {
-                GBUFF_P2D[0][data(NIBBLE_RANGE)] = data(bls12_377_coord_t::width - 1, 0);
-                valid_data = false;
-            }
-        } else if (count_padd_of < num_padd_ops && !padd_output_fifo.empty()) {
-            data = padd_output_fifo.read();
-            count_padd_of += 1;
-            valid_data = true;
-            // Check for last bucket element here.
-            // update padd count here
-            // if last element, put in B_i and change valid to false
-            padd_count[data(NIBBLE_RANGE)] += 1;
-            if (padd_count[data(NIBBLE_RANGE)] == count_B[data(NIBBLE_RANGE)] - 1) {
-                GBUFF_P2D[0][data(NIBBLE_RANGE)] = data(1131 - 1, 0);
-                valid_data = false;
-            }
-        } else
-            valid_data = false;
-
-        if (valid_data) {
-            nibble_K = data(NIBBLE_RANGE);
-            if (nibble_K != 0) {
-                if (fill_B[nibble_K]) {
-                    B[nibble_K].write(data(bls12_377_coord_t::width - 1, 0));
-                    std::cout << "[Bucket " << nibble_K << "] "
-                              << bls12_377_p(data(bls12_377_coord_t::width - 1, 0)).x << "\n";
-                } else {
-                    OVFIFO.write(data);
-                    std::cout << "[OVFIFO " << nibble_K << "] "
-                              << bls12_377_p(data(bls12_377_coord_t::width - 1, 0)).x << "\n";
-                }
-                fill_B[nibble_K] = !fill_B[nibble_K];
-            }
-        }
-    }
-
-    // todo: use splitting as shown in example of ug1399, but conditional data
-    // duplication
-    bls12_377_coord_t result;
-    fp_t p_x, p_y, p_z;
-    u32 b_data;
-
-msm_arr_dataflow_3:
-    for (int i = 0; i < num_padd_ops; i++) {
-        data = OVFIFO.read();
-        nibble_K = data(NIBBLE_RANGE);
-        result = B[nibble_K].read();
-        CFIFO.write((nibble_K, result, data(bls12_377_coord_t::width - 1, 0)));
-    }
+    // TODO: scheduler process: arbitration between CFIFO's and pass to padd unit, add metadata
+    // before sending to padd unit.
 
     // ---- Padd operations ----
     fp_t iter_padd_ops = 0;
     bls12_377_p sum(0, 1, 0);
     fp_t p1_x, p1_y, p1_z, p2_x, p2_y, p2_z;
+    bool padd_done = false, valid_data = false;
+    N_t padd_count[16] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
 msm_arr_dataflow_4:
-    for (int i = 0; i < num_padd_ops; i++) {
-        (nibble_K, p1_x, p1_y, p1_z, p2_x, p2_y, p2_z) = CFIFO.read();
-        bls12_377_p a(p1_x, p1_y, p1_z), b(p2_x, p2_y, p2_z);
-        sum = padd(a, b);
-        iter_padd_ops++;
-        padd_output_fifo.write((nibble_K, sum.x, sum.y, sum.z));
+    // for (N_t i = 0; i < num_padd_ops[0]; i++) {
+    //     (nibble_K, p1_x, p1_y, p1_z, p2_x, p2_y, p2_z) = CFIFO.read();
+    //     bls12_377_p a(p1_x, p1_y, p1_z), b(p2_x, p2_y, p2_z);
+    //     sum = padd(a, b);
+    //     iter_padd_ops++;
+    //     padd_output_fifo_0.write((nibble_K, sum.x, sum.y, sum.z));
+    // }
+
+    while (!padd_done) {
+#pragma HLS allocation function instances = padd limit = 1
+        if (padd_count[0] < num_padd_ops[0] && !CFIFO_0.empty()) {
+            (nibble_K, p1_x, p1_y, p1_z, p2_x, p2_y, p2_z) = CFIFO_0.read();
+            bls12_377_p a(p1_x, p1_y, p1_z), b(p2_x, p2_y, p2_z);
+            sum = padd(a, b);
+            padd_count[0] = padd_count[0] + 1;
+            padd_output_fifo_0.write((nibble_K, sum.x, sum.y, sum.z));
+        } else if (padd_count[1] < num_padd_ops[1] && !CFIFO_1.empty()) {
+            (nibble_K, p1_x, p1_y, p1_z, p2_x, p2_y, p2_z) = CFIFO_1.read();
+            bls12_377_p a(p1_x, p1_y, p1_z), b(p2_x, p2_y, p2_z);
+            sum = padd(a, b);
+            padd_count[1] = padd_count[1] + 1;
+            padd_output_fifo_1.write((nibble_K, sum.x, sum.y, sum.z));
+        } else {
+            padd_done = (padd_count[0] == num_padd_ops[0]) && (padd_count[1] == num_padd_ops[1]);
+        }
     }
+    // TODO: routing required for padd output to respective bucket process
+
+    // -----------------------------------------------------------------------
 
     /*
      * Algorithm Loop 2: Combining the bucket partial sums
