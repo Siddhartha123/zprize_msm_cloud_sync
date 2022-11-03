5c102816279ae6c95353a4573bdca7ac7c6e3549
Ippili Sidhartha Kumar <iskumar789@gmail.com>
2022-11-03 08:07:54 +0530

    scratch pad based design

--- hls_zprize_msm/src/msm.h
+++ hls_zprize_msm/src/msm.h
@@ -9,6 +9,8 @@
 #include "hls_stream.h"
 
 #define NUM_POINTS 256
+#define SCRATCHPAD_SIZE 1024
+#define LOG_SCRATCHPAD_SIZE 10
 
 /*
 N_t is used to hold numbers of the order of the MSM size.
@@ -70,9 +72,19 @@ struct bucket {
 #define NIBBLE_RANGE bls12_377_coord_k_t::width - 1, bls12_377_coord_k_t::width - CHUNK_SIZE
 
 typedef ap_uint<bls12_377_coord_t::width + CHUNK_SIZE> bls12_377_coord_k_t;
+typedef ap_uint<2 * bls12_377_coord_t::width> dbl_bls12_377_coord_t;
 typedef ap_uint<2 * bls12_377_coord_t::width + CHUNK_SIZE> dbl_bls12_377_coord_k_t;
 typedef ap_uint<2 * bls12_377_coord_t::width + CHUNK_SIZE + LOG_NUM_CHUNKS>
     dbl_bls12_377_coord_k_chunk_t;
+
+#define NIBBLE_RANGE_IDX bls12_377_idx_k_t::width - 1, bls12_377_idx_k_t::width - CHUNK_SIZE
+
+typedef ap_uint<bls12_377_idx_t::width + CHUNK_SIZE> bls12_377_idx_k_t;
+typedef ap_uint<2 * bls12_377_idx_t::width> dbl_bls12_377_idx_t;
+typedef ap_uint<2 * bls12_377_idx_t::width + CHUNK_SIZE> dbl_bls12_377_idx_k_t;
+typedef ap_uint<2 * bls12_377_idx_t::width + CHUNK_SIZE + LOG_NUM_CHUNKS>
+    dbl_bls12_377_idx_k_chunk_t;
+
 typedef ap_uint<CHUNK_SIZE> chunk_t;
 typedef ap_uint<REM_CHUNK_SIZE> rem_chunk_t;
 
@@ -81,12 +93,19 @@ void bucket_unit(hls::stream<bls12_377_coord_k_t> &BFIFO_1,
                  N_t count_B[TWO_RAISED_CHUNK_SIZE], hls::stream<dbl_bls12_377_coord_k_t> &CFIFO,
                  bls12_377_coord_t B_i[TWO_RAISED_CHUNK_SIZE]);
 
-// bls12_377_p msm_arr(fp_t P_arr_x[NUM_POINTS], fp_t P_arr_y[NUM_POINTS], fp_t P_arr_z[NUM_POINTS],
-//              fr_t K_arr[NUM_POINTS], hls::stream<N_t> &count_stream, N_t total_num_padd_ops);
+void msm_arr(fr_t K_arr[NUM_POINTS], N_t cnt_bucket_chunks[NUM_CHUNKS][TWO_RAISED_CHUNK_SIZE],
+             hls::stream<N_t> &num_padd_ops, N_t total_num_padd_ops,
+             bls12_377_idx_t GBUFF_P2D[NUM_CHUNKS][TWO_RAISED_CHUNK_SIZE],
+             hls::stream<dbl_bls12_377_idx_k_chunk_t> &padd_input_bucket_fifo,
+             hls::stream<bls12_377_idx_k_t> padd_output_fifo[NUM_CHUNKS]);
+
+void top(fp_t P_arr_x[NUM_POINTS], fp_t P_arr_y[NUM_POINTS], fp_t P_arr_z[NUM_POINTS],
+         fr_t K_arr[NUM_POINTS], N_t cnt_bucket_chunks[NUM_CHUNKS][TWO_RAISED_CHUNK_SIZE],
+         hls::stream<N_t> &num_padd_ops, N_t total_num_padd_ops);
 bls12_377_p msm_reference(hls::stream<fp_t_stream> &K, hls::stream<bls12_377_p_stream> &P);
 
 extern bls12_377_p P_arr[];
-extern fp_t K_arr[];
+// extern fp_t K_arr[];
 
 void printp2k(chunk_t, bls12_377_p p1, bls12_377_p p2);
 
